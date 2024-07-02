2cf19499723c67ca088eab1f607ad10563773c21
yair_rg <yairramirezgaona@gmail.com>
2024-07-02 16:56:38 -0600

    Estilos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,7 +1,14 @@
 import './CreateTodoButton.css'
 function CreateTodoButton() {
   return (
-   <button className="CreateTodoButton"> + </button>
+   <button className="CreateTodoButton"
+   onClick={(event)=>{
+    console.log('le diste click')
+    console.log(event)
+
+   }}
+   
+   > + </button>
   )
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,13 @@
-import './TodoSearch.css'
-export  function TodoSearch() {
+import "./TodoSearch.css";
+export function TodoSearch() {
   return (
-    <input className='TodoSearch' placeholder='Buscar...'></input>
-  )
+    <input
+      className="TodoSearch"
+      placeholder="Buscar..."
+      onChange={(event) => {
+        console.log(event.target);
+        console.log(event.target.value)
+      }}
+    ></input>
+  );
 }
-
